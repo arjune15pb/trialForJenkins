2fdecc3b05d863e535e4cc0402b55dc4dac91fe9
arjun <karnarjun2000@gmail.com>
2025-10-05 22:15:55 +0530

    commiting2

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
         System.out.println("This is java program");
         System.out.println("Output from java program");
         System.out.println(new Date());
-        System.out.println("Commiting1")
+        System.out.println("Commiting1");
 
     }
 }
